ef6cb878f65b57e431ab5c7d1f1736ee04709abb
Yogesh Khater <yogeshjain999@gmail.com>
2023-11-08 14:50:57 +0530

    Add algolia docsearch

---
+++ app/javascript/docsearch.js
@@ -0,0 +1,8 @@
+import '@docsearch/js';
+
+docsearch({
+  container: '#docsearch',
+  appId: 'G0NR418AM9',
+  indexName: 'trailblazer-front',
+  apiKey: 'd2c4b4349d41ab05067c58e954c3e2c9'
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
+pin_all_from "app/javascript"
+
 pin "application", preload: true
 pin "anchor-js", preload: true # @5.0.0
-pin "navigations"
-# pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.8.0
-# pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
-pin "highlight.js/lib/core", to: "https://ga.jspm.io/npm:highlight.js@11.8.0/es/core.js"
-pin "highlight.js/lib/languages/ruby", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/ruby.min.js"
+pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.8.0
+pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
 pin "jquery" # @3.7.1
-
 pin "jquery.parallax-scroll"
+pin "@docsearch/js", to: "docsearch@3.js" # @3.5.2
