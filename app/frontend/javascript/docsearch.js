5443d7935240139dbf7e836e787c1ea2fa82ec7c
Alfonso Uceda <alfonso@hubbado.com>
2025-05-23 15:30:55 +0200

    Algolia docsearch button is styled

--- app/frontend/javascript/docsearch.js
+++ app/frontend/javascript/docsearch.js
@@ -2,7 +2,6 @@ import docsearch from '@docsearch/js';
 
 import '@docsearch/css';
 
-// TODO: remove, currently unused function.
 function my_transformItems(items) {
     return items.map((item) => {
       return {
@@ -17,6 +16,4 @@ docsearch({
   appId: 'LEN50BR1UK',
   indexName: 'trailblazer-website-24',
   apiKey: 'b1f311e280db1c40850ee82e08eb3449',
-
-  // transformItems: my_transformItems,
 });
